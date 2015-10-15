23a7674f913a8c4a96a09303e7696fe7ea795e6e
aadithya <aadithyabk@gmail.com>
2015-10-14 18:55:04 -0700

    Update WindEffects.cpp

--- Wind Effects/WindEffects.cpp
+++ Wind Effects/WindEffects.cpp
@@ -2,35 +2,6 @@
 namespace PE {
 namespace Components {
 
-PE_IMPLEMENT_CLASS1(RootSceneNode, SceneNode);
-
-Handle RootSceneNode::s_hTitleInstance;
-Handle RootSceneNode::s_hInstance;
-Handle RootSceneNode::s_hCurInstance;
-
-void RootSceneNode::Construct(PE::GameContext &context, PE::MemoryArena arena)
-{
-	Handle h("ROOT_SCENE_NODE", sizeof(RootSceneNode));
-	RootSceneNode *pRootSceneNode = new(h) RootSceneNode(context, arena, h);
-	pRootSceneNode->addDefaultComponents();
-	pRootSceneNode->m_toggle=0;
-	pRootSceneNode->m_intensity=1;
-	SetInstance(h);
-	
-	s_hTitleInstance = Handle("ROOT_SCENE_NODE", sizeof(RootSceneNode));
-	RootSceneNode *pTitleRootSceneNode = new(s_hTitleInstance) RootSceneNode(context, arena, h);
-	pTitleRootSceneNode->addDefaultComponents();
-	SetTitleAsCurrent();
-}
-
-void RootSceneNode::addDefaultComponents()
-{
-	SceneNode::addDefaultComponents();
-
-	PE_REGISTER_EVENT_HANDLER(Events::Event_GATHER_DRAWCALLS, RootSceneNode::do_GATHER_DRAWCALLS);
-	PE_REGISTER_EVENT_HANDLER(Events::Event_GATHER_DRAWCALLS_Z_ONLY, RootSceneNode::do_GATHER_DRAWCALLS);
-}
-
 void RootSceneNode::do_GATHER_DRAWCALLS(Events::Event *pEvt)
 {
 	DrawList *pDrawList = NULL;
@@ -41,8 +12,6 @@ void RootSceneNode::do_GATHER_DRAWCALLS(Events::Event *pEvt)
 	SceneNode *pRoot = RootSceneNode::Instance();
 	Events::Event_GATHER_DRAWCALLS *pDrawEvent = zOnly ? NULL : (Events::Event_GATHER_DRAWCALLS *)(pEvt);
 	Events::Event_GATHER_DRAWCALLS_Z_ONLY *pZOnlyDrawEvent = zOnly ? (Events::Event_GATHER_DRAWCALLS_Z_ONLY *)(pEvt) : NULL;
-
-	
 
 	bool setGlobalValues = true;
 	if (!zOnly && pDrawEvent->m_drawOrder != EffectDrawOrder::First)
@@ -72,16 +41,11 @@ void RootSceneNode::do_GATHER_DRAWCALLS(Events::Event *pEvt)
 				m_toggle = 0;
 		}
 		
-
-		//SETTING WIND VALUES & SOURCES
-		Vector3 _WindEffect = Vector3(0.0f, 0.0f, 0.0f);
-		float _Intensity = 100;
-
 		//CAMERA
 		Handle hCam = CameraManager::Instance()->getActiveCameraHandle();
 		CameraSceneNode *pcam = CameraManager::Instance()->getActiveCamera()->getCamSceneNode();
 
-		Vector3 _CameraPosition = pcam->m_base.getPos();
+		Vector3 cameraPosition = pcam->m_base.getPos();
 
 		static float x = 0;
 		x += 0.05f;
@@ -90,22 +54,21 @@ void RootSceneNode::do_GATHER_DRAWCALLS(Events::Event *pEvt)
 		{
 			for(int i=0 ; i<m_SoldierCount ; i++)
 			{
-				SceneNode* _Soldier = m_SoldierSceneNodes[i];
+				SceneNode* soldier = m_SoldierSceneNodes[i];
 				
 				//Source of wind is the position of soldier
 				if(i == 0)
 				{
-					
-					p->m_data.windSource0[0] = _Soldier->m_base.getPos().m_x;
-					p->m_data.windSource0[1] = _Soldier->m_base.getPos().m_y;
-					p->m_data.windSource0[2] = _Soldier->m_base.getPos().m_z;
+					p->m_data.windSource0[0] = soldier->m_base.getPos().m_x;
+					p->m_data.windSource0[1] = soldier->m_base.getPos().m_y;
+					p->m_data.windSource0[2] = soldier->m_base.getPos().m_z;
 					p->m_data.windSource0[3] = 1.0f;
 				}
 				else
 				{
-					p->m_data.windSource1[0] = _Soldier->m_base.getPos().m_x;
-					p->m_data.windSource1[1] = _Soldier->m_base.getPos().m_y;
-					p->m_data.windSource1[2] = _Soldier->m_base.getPos().m_z;
+					p->m_data.windSource1[0] = soldier->m_base.getPos().m_x;
+					p->m_data.windSource1[1] = soldier->m_base.getPos().m_y;
+					p->m_data.windSource1[2] = soldier->m_base.getPos().m_z;
 					p->m_data.windSource1[3] = 1.0f;
 				}
 			}
@@ -124,9 +87,9 @@ void RootSceneNode::do_GATHER_DRAWCALLS(Events::Event *pEvt)
 			p->m_data.windSource1[3] = 0.0f;
 		}
 		//The third wind source is the camera itself.
-		p->m_data.windSource2[0] = _CameraPosition.m_x;
-		p->m_data.windSource2[1] = _CameraPosition.m_y;
-		p->m_data.windSource2[2] = _CameraPosition.m_z;
+		p->m_data.windSource2[0] = cameraPosition.m_x;
+		p->m_data.windSource2[1] = cameraPosition.m_y;
+		p->m_data.windSource2[2] = cameraPosition.m_z;
 		p->m_data.windSource2[3] = 1.0f;
 		p->m_data.keyToggle[0] = m_toggle;
 		p->m_data.keyToggle[1] = m_intensity;
@@ -178,28 +141,12 @@ void RootSceneNode::do_GATHER_DRAWCALLS(Events::Event *pEvt)
 			m_intensity-=0.05;
 		}
 
-
-
-
-		//SETTING WIND VALUES & SOURCES
-
-			Vector3 _WindEffect = Vector3(0.0f, 0.0f, 0.0f);
-		float _Intensity = 100;
-
 		//CAMERA
 		Handle hCam = CameraManager::Instance()->getActiveCameraHandle();
 		CameraSceneNode *pcam = CameraManager::Instance()->getActiveCamera()->getCamSceneNode();
 
-		Vector3 _CameraPosition = pcam->m_base.getPos();
+		Vector3 cameraPosition = pcam->m_base.getPos();
 
-		static float x = 0;
-		x += 0.05f;
-
-
-		//SineWave
-		/*float y1 = 10 + sin(x) * 10;
-		float y2 = 10 + sin(x) * 10;*/
-		
 		if(m_SoldierCount > 0)
 		{
 			for(int i=0 ; i<m_SoldierCount ; i++)
@@ -236,9 +183,9 @@ void RootSceneNode::do_GATHER_DRAWCALLS(Events::Event *pEvt)
 			psvPerObjectGroup->m_data.windSource1[3] = 0.0f;
 		}
 
-		psvPerObjectGroup->m_data.windSource2[0] = _CameraPosition.m_x;
-		psvPerObjectGroup->m_data.windSource2[1] = _CameraPosition.m_y;
-		psvPerObjectGroup->m_data.windSource2[2] = _CameraPosition.m_z;
+		psvPerObjectGroup->m_data.windSource2[0] = cameraPosition.m_x;
+		psvPerObjectGroup->m_data.windSource2[1] = cameraPosition.m_y;
+		psvPerObjectGroup->m_data.windSource2[2] = cameraPosition.m_z;
 		psvPerObjectGroup->m_data.windSource2[3] = 1.0f;
 
 		psvPerObjectGroup->m_data.keyToggle[0] = m_toggle;
